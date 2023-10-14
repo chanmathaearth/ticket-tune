beea4e93afea6ee8c358ee5abf64f4ad67d19984
chanmathaearth <chanmathaearth@gmail.com>
2023-10-15 02:30:26 +0700

    Update topbar.component.ts

--- src/app/component/topbar/topbar.component.ts
+++ src/app/component/topbar/topbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGModule } from 'src/app/modules/primeng.module';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
+  standalone: true,
+  imports: [
+    PrimeNGModule,
+  ]
 })
-export class TopbarComponent {
 
-}
+export class TopbarComponent {}
+export class ButtonTextDemo {}
+
+// ngOnInit(): void {  }
