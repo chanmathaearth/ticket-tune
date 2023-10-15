e949ea1db0c9a5792f3a5131ced81261d4ccf0a2
Gunt Thanadol <109953241+Thanadol65070091@users.noreply.github.com>
2023-10-15 17:15:33 +0700

    pro

--- src/app/page/profile/profile.component.ts
+++ src/app/page/profile/profile.component.ts
@@ -1,38 +1,57 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
-
-// import { ProfileService } from 'src/app/service/profile/profile.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-  userId: string = "";
-  user: any = [];
+export class ProfileComponent implements OnInit {
+  formData: FormGroup = new FormGroup({});
+  @ViewChild('fileInput') fileInput!: ElementRef;
 
-  formData: FormGroup = new FormGroup({
-    first_name: new FormControl(''),
-    last_name: new FormControl(''),
-    email: new FormControl(''),
-    tel: new FormControl(''),
-    password: new FormControl(''),
-  });
-
-  // constructor(
-  //   private profileService: ProfileService,
-  // ) {
-  //   let item = JSON.parse(localStorage.getItem('user')!) || [];
-  //   this.userId = item.user.user_id;
-  //   this.getProfile()
-  // }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
 
-  // getProfile() {
-  //   this.profileService.getProfile(this.userId).subscribe(profile => {
-  //     this.formData.patchValue(profile);
-  //   });
-  // }
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.formData = this.fb.group({
+      first_name: ['', [Validators.required]],
+      last_name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      tel: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  changeProfilePicture() {
+    // Trigger the hidden file input
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      // Handle the selected file, e.g., upload it to the server
+      const selectedFile = inputElement.files[0];
+      // You can implement an upload function here
+    }
+  }
+
+  saveProfile() {
+    if (this.formData.valid) {
+      // Implement your save functionality here
+      console.log(this.formData.value);
+    }
+  }
+
+  navigateToPayment() {
+    // Use the router to navigate to the payment page
+    this.router.navigate(['/payment']);
+  }
+  
 }
