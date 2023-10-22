4882f0cac83cabdbb96883322cda35279fb21e49
chanmathaearth <chanmathaearth@gmail.com>
2023-10-22 23:24:05 +0700

    Update home.component.ts

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ConcertService } from 'src/app/service/concert/concert.service';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +7,54 @@ import { ConcertService } from 'src/app/service/concert/concert.service';
 })
 export class HomeComponent {
   searchText: string = '';
-  listConcert: any = []
+  listConcert: any[] = [
+    {
+      concert_id: 1,
+      name: 'OTHER WORLD',
+      img_url: 'https://s3-ap-southeast-1.amazonaws.com/tm-img-poster-event/e0d3d4305a0111ee911101117567899b.jpg?opt=mild&resize=w200,h290',
+      location: 'REXKL'
+    },
+    {
+      concert_id: 2,
+      name: 'BUD LIVEHOUSE:OCT 2023',
+      img_url: 'https://s3-ap-southeast-1.amazonaws.com/tm-img-poster-event/0af42db05dc111ee911101117567899b.jpg?format=basic&resize=w425,h610',
+      location: 'Lido Connect'
+    },
+    {
+      concert_id: 3,
+      name: 'ANATOMY RABBIT',
+      img_url: 'https://s3-ap-southeast-1.amazonaws.com/tm-img-poster-event/13c7c4906bf611ee911101117567899b.jpg?format=basic&resize=w425,h610',
+      location: 'Blueprint Livehouse'
+    },
+    {
+      concert_id: 4,
+      name: 'Office Syndrome + YOURHEART + ALISON',
+      img_url: 'https://s3-ap-southeast-1.amazonaws.com/tm-img-poster-event/cb9c01a06c2611ee911101117567899b.jpg?format=basic&resize=w425,h610',
+      location: 'Blueprint Livehouse'
+    },
+    {
+      concert_id: 5,
+      name: 'DOOR PLANT + Chucky Factory Land + But Hate Panic',
+      img_url: 'https://s3-ap-southeast-1.amazonaws.com/tm-img-poster-event/b2a6bf3057ba11ee911101117567899b.jpg?opt=mild&resize=w200,h290',
+      location: 'Blueprint Livehouse'
+    },
+  ];
+  filteredConcerts: any[] = [];
 
-  constructor(private concertService: ConcertService) {
-    this.getConscert();
+  constructor() {
+    this.filteredConcerts = this.listConcert;
   }
-  getConscert() { 
-    this.concertService.getConcert().subscribe(res => {
-      this.listConcert = res
-    })
+
+  search() {
+    if (this.searchText) {
+      this.filteredConcerts = this.listConcert.filter((concert: any) => {
+        return (
+          concert.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+          concert.location.toLowerCase().includes(this.searchText.toLowerCase())
+        );
+      });
+    } else {
+      this.filteredConcerts = this.listConcert;
+    }
   }
-  search() { }
 }
